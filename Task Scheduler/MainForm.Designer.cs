e18ffba6a02897fb4710d455994b4cbaab1637d4
Sầm Đức Vũ <phibao37@hotmail.com>
2017-02-15 22:26:56 +0700

    Add default interface

--- Task Scheduler/MainForm.Designer.cs
+++ Task Scheduler/MainForm.Designer.cs
@@ -28,23 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TabPage tabPage1;
+            System.Windows.Forms.TabPage tabPage2;
+            System.Windows.Forms.Panel panel1;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCenter = new System.Windows.Forms.Button();
+            this.tabControlTask = new System.Windows.Forms.TabControl();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            tabPage1 = new System.Windows.Forms.TabPage();
+            tabPage2 = new System.Windows.Forms.TabPage();
+            panel1 = new System.Windows.Forms.Panel();
+            panel1.SuspendLayout();
+            this.tabControlTask.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new System.Drawing.Point(4, 22);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            tabPage1.Size = new System.Drawing.Size(554, 191);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Cơ bản";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new System.Drawing.Point(4, 22);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            tabPage2.Size = new System.Drawing.Size(554, 191);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Thời gian";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(this.button2);
+            panel1.Controls.Add(this.button1);
+            panel1.Controls.Add(this.buttonCenter);
+            panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            panel1.Location = new System.Drawing.Point(5, 222);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(562, 37);
+            panel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button2.Location = new System.Drawing.Point(358, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 31);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Hủy bỏ";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(98, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Trước đó";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // buttonCenter
+            // 
+            this.buttonCenter.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonCenter.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.buttonCenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.buttonCenter.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonCenter.Location = new System.Drawing.Point(229, 3);
+            this.buttonCenter.Name = "buttonCenter";
+            this.buttonCenter.Size = new System.Drawing.Size(102, 31);
+            this.buttonCenter.TabIndex = 0;
+            this.buttonCenter.Text = "Tạo mới";
+            this.buttonCenter.UseVisualStyleBackColor = false;
+            // 
+            // tabControlTask
+            // 
+            this.tabControlTask.Controls.Add(tabPage1);
+            this.tabControlTask.Controls.Add(tabPage2);
+            this.tabControlTask.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControlTask.Location = new System.Drawing.Point(5, 5);
+            this.tabControlTask.Name = "tabControlTask";
+            this.tabControlTask.SelectedIndex = 0;
+            this.tabControlTask.Size = new System.Drawing.Size(562, 217);
+            this.tabControlTask.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(5, 259);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.monthCalendar1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.splitContainer1.Size = new System.Drawing.Size(562, 233);
+            this.splitContainer1.SplitterDistance = 372;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(2, 1);
+            this.monthCalendar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monthCalendar1.Location = new System.Drawing.Point(0, 0);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 497);
+            this.ClientSize = new System.Drawing.Size(572, 497);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(panel1);
+            this.Controls.Add(this.tabControlTask);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
+            this.Padding = new System.Windows.Forms.Padding(5);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Task Scheduler";
+            panel1.ResumeLayout(false);
+            this.tabControlTask.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControlTask;
+        private System.Windows.Forms.Button buttonCenter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
     }
 }
 
--- Task Scheduler/MainForm.cs
+++ Task Scheduler/MainForm.cs
@@ -6,15 +6,38 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace sdv
 {
+   
+    /// <summary>
+    /// Main form for Task Scheduler application
+    /// </summary>
     public partial class MainForm : Form
     {
+
+        /// <summary>
+        /// Create new form object
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
+            setupComponent();
+        }
+
+
+        /// <summary>
+        /// Do configuration for created GUI component
+        /// </summary>
+        private void setupComponent()
+        {
+            foreach (TabPage page in tabControlTask.TabPages)
+            {
+                page.Enabled = false;
+                Debug.WriteLine("Disabling " + page);
+            }
         }
     }
 }
