39763d2941f4a502d9f8498893fe9ad2468ba95a
Sầm Đức Vũ <phibao37@hotmail.com>
2017-02-15 20:56:26 +0700

    Refactor project

--- Task Scheduler/MainForm.Designer.cs
+++ Task Scheduler/MainForm.Designer.cs
@@ -28,9 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(520, 497);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Task Scheduler";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
